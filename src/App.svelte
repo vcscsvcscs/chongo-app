99325fe3cf31933239e2621e5cbc8e0265e1e8d1
Berecz Dániel <c4q4kv@inf.elte.hu>
2022-03-10 12:43:15 +0100

    Regisztráció próba

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,51 @@
 <script>
 	export let name;
+	import { useForm, Hint, HintGroup, validators, required, minLength, email } from "svelte-use-form@2.0.0";
+	import { passwordMatch, containNumbers } from "./customValidators";
+	
+	const form = useForm();
+	
+	const requiredMessage = "This field is required";
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<form use:form>
+		<h1>
+			Registration
+		</h1>
+		<label for="email">Email</label>
+		<input type="email" name="email" use:validators={[required, email]} />
+		<HintGroup for="email">
+			<Hint on="required">{requiredMessage}</Hint>
+			<Hint on="email" hideWhenRequired>This must be a valid email</Hint>	
+		</HintGroup>
+
+		<label for="name">Name</label>
+		<input type="text" name="name"  />
+
+		<label for="password">Password</label>
+		<input type="password" name="password" use:validators={[required, minLength(5), containNumbers(2)]} />
+		<HintGroup for="password">
+			<Hint on="required">{requiredMessage}</Hint>
+			<Hint on="minLength" hideWhenRequired let:value>This field must have at least {value} characters.</Hint>	
+			<Hint on="containNumbers" hideWhen="minLength" let:value>
+				This field must contain at least {value} numbers.
+			</Hint>	
+		</HintGroup>
+
+		<label for="passwordConfirmation">Password Confirmation</label>
+		<input type="password" name="passwordConfirmation" use:validators={[required, passwordMatch]} />
+		<HintGroup for="passwordConfirmation">
+			<Hint on="required">{requiredMessage}</Hint>
+			<Hint on="passwordMatch" hideWhenRequired>Passwords do not match</Hint>	
+		</HintGroup><br />
+
+		<button disabled={!$form.valid} on:click|preventDefault>
+			Submit
+		</button>
+	</form>
 </main>
 
 <style>
