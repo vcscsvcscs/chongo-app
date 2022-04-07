492c6543669e60fa8a7f5ffaec2f834e57509c89
Vargha Csongor <v5inla@inf.elte.hu>
2022-04-08 00:41:31 +0200

    LogReg re design

--- src/components/LogReg.svelte
+++ src/components/LogReg.svelte
@@ -1,45 +1,26 @@
 <script>
- 	import Login from '../components/LogReg/Login.svelte';
-	import Register from '../components/LogReg/Register.svelte';
+	import Login from "../components/LogReg/Login.svelte";
+	import Register from "../components/LogReg/Register.svelte";
 	const requiredMessage = "This field is required";
-	let change="Registration";
-	let login=true;
-    function handleClick(event) {
-        login=!login;
-		if(login==true)
-			change="Registration";
-		else
-			change="Login";
-    }
+	let change = "Registration";
+	let login = true;
+	function handleClick(event) {
+		login = !login;
+		if (login == true) change = "Registration";
+		else change = "Login";
+	}
 </script>
-<div id="logincontent">
-		<div id="Background">
-		{#if login}
-			<Login />
-		{:else}
-			<Register/>
-		{/if}
-		</div>
-		<div id="Change"><button on:click={handleClick}>{change}</button></div>
-        </div>
-<style>
-	#Background{
-		height: 50%;
-		width: 50%;
-		margin-top: auto;
-		background-color: rgba(0, 0, 0, 0.5);
 
-	}
-	#Change{
-		height: 10%;
-		width: 50%;
-		margin-bottom: auto;
-		background-color: rgba(0, 0, 0, 0.5);
-	}
-    #logincontent{
-        display: flex;
-        flex-direction:column;
-        align-items: center;
-        height: 100vh;
-    }
-</style>
+<link rel="icon" type="image/png" sizes="500x500" href="assets/img/logo.png" />
+<link rel="stylesheet" href="assets/css/styles.css" />
+<link rel="stylesheet" href="assets/css/Login-Form-Dark.css" />
+<section class="login-dark">
+	{#if login}
+		<Login />
+	{:else}
+		<Register />
+	{/if}
+	<button id="changer" on:click={handleClick} class="btn btn-primary d-block"
+		>{change}</button
+	>
+</section>
